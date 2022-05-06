b0e64ead4c3caa6f96832a6eda4f74d7668195e1
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-05-06 21:36:59 +0800

    Added dotenv and deploy script to binance testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const wallets = [process.env.HOLDER, process.env.WALLET];
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +20,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: wallets
+    }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+  },
+  paths: {
+    artifacts: "./client/artifacts"
+  }
 };
