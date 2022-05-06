36c81800701d01e7d0a73b1aa3c40ab0a0e49207
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-05-06 19:24:23 +0800

    Added deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const ethers = hre.ethers;
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -12,14 +13,23 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
+  const [holder, wallet1] = await ethers.getSigners();
+  console.log(`holder address: ${holder.address}, wallet1 address: ${wallet1.address}`);
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Wbtc = await ethers.getContractFactory("Wbtc", holder);
+  const wbtc = await Wbtc.deploy();
+  await wbtc.deployed();
+  console.log("Wbtc deployed to:", wbtc.address);
 
-  await greeter.deployed();
+  // Deploying staking contract
+  const Staking = await ethers.getContractFactory("Staking", wallet1);
+  const staking = await Staking.deploy();
 
-  console.log("Greeter deployed to:", greeter.address);
+  // whitelisting wbtc to staking contract
+  await staking.whitelistCoin('wbtc', wbtc.address);
+  const whitelistedAdd = await staking.whitelistedCoin('wbtc');
+  console.log('whitelistedCoin: ', whitelistedAdd);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
