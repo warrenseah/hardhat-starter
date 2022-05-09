bc89706a243cd4f9a48074362f178b6b2ca80fdc
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-05-09 18:43:46 +0800

    Added staking functionailities

--- contracts/RewardsToken.sol
+++ contracts/RewardsToken.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/IAccessControlEnumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -9,35 +10,79 @@ import "./RewardsToken.sol";
 
 contract Staking is Ownable {
     RewardsToken public rewardsToken;
-    mapping(string => address) public whitelistedCoin;
-    mapping(address => mapping(string => uint256)) public stakingBalance;
+    address public paymentToken;
+
+    // userAddress => stakingBalance
+    mapping(address => uint256) public stakingBalance;
+    // userAddress => isStaking boolean
+    mapping(address => bool) public isStaking;
+    // userAddress => timestamp
+    mapping(address => uint256) public startTime;
+    // userAddress => rewardsBalance
+    mapping(address => uint256) public rewardsBalance;
+
+    // Events
+    event Stake(address indexed from, uint256 amount);
+    event Unstake(address indexed from, uint256 amount);
+    event YieldWithdraw(address indexed to, uint256 amount);
     
     constructor() {
         rewardsToken = new RewardsToken();
     }
 
-    function whitelistCoin(string memory _coin, address _coinAddress) external onlyOwner {
-        whitelistedCoin[_coin] = _coinAddress;
+    function changePaymentAddress(address _coinAddress) external onlyOwner {
+        paymentToken = _coinAddress;
     }
 
-    function depositCoin(string memory _coin, uint256 _amount) external {
+    function depositCoin(uint256 _amount) external onlyAcceptedToken {
         require(_amount > 0, 'Amount cannot be zero');
-        IERC20 wbtc = IERC20(whitelistedCoin[_coin]);
-        uint256 available = wbtc.balanceOf(msg.sender);
+        IERC20 token = IERC20(paymentToken);
+        uint256 available = token.balanceOf(msg.sender);
         require(available >= _amount, 'Insufficient coin balance');
-        stakingBalance[msg.sender][_coin] += _amount;
-        IERC20(whitelistedCoin[_coin]).transferFrom(msg.sender, address(this), _amount);
+
+        if(isStaking[msg.sender] == true) {
+            uint256 toTransfer = calculateYieldTotal(msg.sender);
+            rewardsBalance[msg.sender] += toTransfer;
+        }
+
+        stakingBalance[msg.sender] += _amount;
+        startTime[msg.sender] = block.timestamp;
+        isStaking[msg.sender] = true;
+        IERC20(paymentToken).transferFrom(msg.sender, address(this), _amount);
+        emit Stake(msg.sender, _amount);
     }
 
-    function withdrawCoin(string memory _coin, uint256 _amount) external {
+    function withdrawCoin(uint256 _amount) external {
         require(_amount > 0, 'Amount cannot be zero');
-        require(stakingBalance[msg.sender][_coin] >= _amount, 'Insufficient coin staking balance');
+        require(stakingBalance[msg.sender] >= _amount, 'Nothing to unstake');
 
-        stakingBalance[msg.sender][_coin] -= _amount;
-        IERC20(whitelistedCoin[_coin]).transfer(msg.sender, _amount);
+        uint256 yieldTransfer = calculateYieldTotal(msg.sender);
+        delete startTime[msg.sender]; // bug fix
+
+        stakingBalance[msg.sender] -= _amount;
+        IERC20(paymentToken).transfer(msg.sender, _amount);
+        rewardsBalance[msg.sender] += yieldTransfer;
+
+        if(stakingBalance[msg.sender] == 0) {
+            isStaking[msg.sender] = false;
+        }
+        emit Unstake(msg.sender, _amount);
+
     }
 
-    
+    function withdrawYield() public {
+        uint256 toTransfer = calculateYieldTotal(msg.sender);
+        require(toTransfer > 0, "Cannot withdraw 0");
+        require(rewardsBalance[msg.sender] > 0, "Nothing to withdraw");
+        
+        uint256 oldBalance = rewardsBalance[msg.sender];
+        rewardsBalance[msg.sender] = 0;
+        toTransfer += oldBalance;
+
+        delete startTime[msg.sender];
+        rewardsToken.mint(msg.sender, toTransfer);
+        emit YieldWithdraw(msg.sender, toTransfer);
+    }
 
     // Access Functions
     function grantRDSRole(bytes32 _role, address _account) external onlyOwner {
@@ -50,5 +95,29 @@ contract Staking is Ownable {
 
     function getRDSRoleCount(bytes32 _role) view external returns(uint256) {
         return IAccessControlEnumerable(rewardsToken).getRoleMemberCount(_role);
+    }
+
+    // Modifiers
+    modifier onlyAcceptedToken() {
+        require(paymentToken != address(0), "Must only work with a accepted payment token");
+        _;
+    }
+
+    // Helper functions
+
+    function _calculateYieldTime(address user) public view returns(uint256) {
+        uint256 end = block.timestamp;
+        uint256 totalTime = end - startTime[user];
+        return totalTime;
+    }
+
+    function calculateYieldTotal(address user) public view returns(uint256) {
+        uint256 time = _calculateYieldTime(user) * 10**18;
+        uint256 dayInSeconds = 86400;
+        uint256 timeInDay = time / dayInSeconds;
+        uint256 rate = 1;
+        uint256 totalStakingPool = IERC20(paymentToken).balanceOf(address(this));
+        uint256 rawYield = (stakingBalance[msg.sender] / totalStakingPool * timeInDay * rate) / 10**18;
+        return rawYield;
     }
 }
